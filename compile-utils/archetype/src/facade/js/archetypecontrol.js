e2279e8f5bf901d194081bb16dbb5d2aedfcacca
JGLeal <jgleal@gmail.com>
2018-07-17 14:31:52 +0200

    sustitución de eventos por métodos heredados

--- compile-utils/archetype/src/facade/js/archetypecontrol.js
+++ compile-utils/archetype/src/facade/js/archetypecontrol.js
@@ -22,25 +22,6 @@ export class {{archetype.plugin.name}}Control extends M.Control {
     let impl = new M.impl.control.{{archetype.plugin.name}}Control();
     super(impl, "{{archetype.plugin.name}}");
 
-    //** código propio */
-    //captura de eventos de des/activación del control
-    this.on(M.evt.ACTIVATED, () => {
-      let div = document.createElement("div");
-      div.id = "msgInfo";
-      div.classList.add("info");
-      div.innerHTML = "Haz doble click sobre el mapa";
-
-      this.map_.getContainer().appendChild(div);
-
-      impl.activateClick(this.map_);
-    });
-
-    this.on(M.evt.DEACTIVATED, () => {
-      let div = document.getElementById("msgInfo");
-      this.map_.getContainer().removeChild(div);
-      impl.deactivateClick(this.map_);
-    });
-
     //captura de customevent lanzado desde impl con coords
     window.addEventListener("mapclicked", e => {
       this.map_.addLabel("Hola Mundo!", e.detail);
@@ -62,7 +43,37 @@ export class {{archetype.plugin.name}}Control extends M.Control {
         });
     });
    }
+  /**
+   * This function is called on the control activation
+   *
+   * @public
+   * @function
+   * @api stable
+   */
+  activate() {
+    super.activate(); //calls super to manage de/activation
+    let div = document.createElement("div");
+    div.id = "msgInfo";
+    div.classList.add("info");
+    div.innerHTML = "Haz doble click sobre el mapa";
+    this.map_.getContainer().appendChild(div);
 
+    this.getImpl().activateClick(this.map_);
+  }
+  /**
+   * This function is called on the control deactivation
+   *
+   * @public
+   * @function
+   * @api stable
+   */
+  deactivate() {
+    super.deactivate(); //calls super to manage de/activation
+    let div = document.getElementById("msgInfo");
+    this.map_.getContainer().removeChild(div);
+
+    this.getImpl().deactivateClick(this.map_);
+  }
    /**
    * This function gets activation button
    *
